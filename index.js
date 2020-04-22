bad58e9009ea7a25bf1b5b72a2965a82b0f39b99
Kotokaze <ryu_micro@outlook.jp>
2020-04-22 15:13:11 +0900

    出力の自然化

--- index.js
+++ index.js
@@ -19,9 +19,11 @@ const server = http.createServer((req, res) => {
         rawData = rawData + chunk;
       }).on('end', () => {
         const decoded = decodeURIComponent(rawData);
-        console.info('[' + now + '] 投稿: ' + decoded);
+        const qs = require('querystring');
+        const answer = qs.parse(decoded);
+        console.info('[' + now + ']' + answer['name']+ 'さんの投票:' + answer['yaki-shabu']);
         res.write('<!DOCTYPE html><html lang="ja"><body><h1>' +
-          decoded + 'が投稿されました</h1></body></html>');
+          answer['name'] + 'さんは'+answer['yaki-shabu']+'投票しました</h1></body></html>');
         res.end();
       });
       break;
